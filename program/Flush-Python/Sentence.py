d685ef7a47ecae638e30dccc1a54f96466249318
PiKaChu-R <827710637@qq.com>
2019-08-15 10:46:38 +0800

    Flush-Python
    Time：2019-08-15
    Detail：具体完成时间为2019-08-14，提交时间为Time。增加流畅的Python的部分程序实现。

--- program/Flush-Python/Sentence.py
+++ program/Flush-Python/Sentence.py
@@ -1,30 +1,95 @@
-Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import re
->>> import reprlib
->>> RE_WORD = re.compile('\w+')
->>> class Swntence:
-	def __init__(self,text):
-		self.text = text
-		self.words = RE_WORD.findall(text)
-	def __getitem__(self,index):
-		return self.words[index]
-	def __len__(self):
-		return len(self.words)
-	def __repr__(self):
-		return 'Sentence(%s)'%reprlib.repr(self.text)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-	
->>> so = Swntence('hello word and you')
->>> so
-Sentence('hello word and you')
->>> so.text
-'hello word and you'
->>> so.words
-['hello', 'word', 'and', 'you']
->>> so = Swntence('"The time has come," the Walrus said,')
->>> so
-Sentence('"The time ha... Walrus said,')
->>> list(so)
-['The', 'time', 'has', 'come', 'the', 'Walrus', 'said']
->>> 
+
+import re
+import reprlib
+RE_WORD = re.compile('\w+')
+
+# 第一版
+
+
+class Sentence1:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __getitem__(self, index):
+        return self.words[index]
+
+    def __len__(self):
+        return len(self.words)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+
+# 第二版本
+class Sentence2:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        return SentenceIterator(self.words)
+
+
+class SentenceIterator:
+    def __init__(self, words):
+        self.words = words
+        self.index = 0
+
+    def __next__(self):
+        try:
+            word = self.words[self.index]
+        except IndexError:
+            raise StopIteration()
+        self.index += 1
+        return word
+
+    def __iter__(self):
+        return self
+
+# 第三版:
+
+
+class Sentence3:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        for word in self.words:
+            yield word
+        return
+
+
+# 第四版
+class Sentence4:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        for match in RE_WORD.finditer(self.text):
+            yield match.group()
+
+
+# 第五版
+class Sentence5:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'Sentence5(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        return (match.group for match in RE_WORD.finditer(self.text))
